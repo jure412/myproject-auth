8dfa47b80e37d767be8b6d7823b47514a6373ac2
jure412 <jur3curk@gmail.com>
2024-03-02 07:55:29 +0100

    Shopping list pages update

--- app/shoppingList/[id]/page.tsx
+++ app/shoppingList/[id]/page.tsx
@@ -1,4 +1,36 @@
+import ItemCheck from "@/app/components/ItemCheck/ItemCheck";
+import ModalButton from "@/app/components/Modal/ModalComponents/ModalButton";
+import StatusUpdate from "@/app/components/StatusUpdate/StatusUpdate";
+import { ModalType } from "@/enums";
+
 const ShoppingItem = async ({ params }: any) => {
-  return <>ShoppingList single</>;
+  // console.log(params.id);
+  const res = await fetch(
+    "http://localhost:3000/api/shoppingList/" + params.id,
+    {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
+  const data = await res.json();
+
+  return (
+    <>
+      <ModalButton modalType={ModalType.CREATE_NEW_ITEM}>
+        Add new Item
+      </ModalButton>
+      <h1>{data?.name}</h1>
+      <StatusUpdate id={params.id} status={data.data.status} />
+      <h2>List</h2>
+      <div>
+        {data &&
+          data?.data.items?.map((item: any, i: number) => (
+            <ItemCheck item={item} key={item.id} />
+          ))}
+      </div>
+    </>
+  );
 };
 export default ShoppingItem;
--- app/shoppingList/page.tsx
+++ app/shoppingList/page.tsx
@@ -1,12 +1,29 @@
 import { ModalType } from "@/enums";
 import ModalButton from "../components/Modal/ModalComponents/ModalButton";
+import ShoppingListCollapse from "../components/ShoppingListCollapse/ShoppingListCollapse";
 
 const ShoppingList = async () => {
+  const res = await fetch("http://localhost:3000/api/shoppingList", {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+  const data = await res.json();
   return (
     <>
-      <ModalButton modalType={ModalType.CREATE_NEW_SHOPPING_LIST}>
+      <ModalButton
+        isDisabled={!data.data.isActiveOpen}
+        modalType={ModalType.CREATE_NEW_SHOPPING_LIST}
+      >
         Create New Shopping List
       </ModalButton>
+      <div>
+        {data &&
+          data?.data.shoppingLists?.map((list: any, i: number) => (
+            <ShoppingListCollapse key={i} list={list} />
+          ))}
+      </div>
     </>
   );
 };
