985cb369bdd5b6f4c0ab93c15b3edd5bc98953df
jure412 <jur3curk@gmail.com>
2024-03-03 10:27:43 +0100

    shoppingListCollapse component

--- app/components/ShoppingListCollapse/ShoppingListCollapse.tsx
+++ app/components/ShoppingListCollapse/ShoppingListCollapse.tsx
@@ -5,78 +5,101 @@ import { FC, useEffect, useState } from "react";
 import styles from "../../global.module.scss";
 import Link from "../Link/Link";
 import Loading from "../Loading/Loading";
-import style from "./ShoppingListCollapse.module.scss";
+import StatusUpdate from "../StatusUpdate/StatusUpdate";
 
-interface ShoppingListCollapseProps {
-  list: {
-    id: string;
-    name: string;
-    status: string;
-  };
+export type List = {
+  id: string;
+  name: string;
+  status: Role;
+  _count: { items: number };
+};
+export interface ShoppingListCollapseProps {
+  list: List;
 }
 
-const ShoppingListCollapse: FC<ShoppingListCollapseProps> = ({ list }: any) => {
-  const [data, setData] = useState<any | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
+const ShoppingListCollapse: FC<ShoppingListCollapseProps> = ({ list }) => {
   const [open, setOpen] = useState(false);
-
-  useEffect(() => {
-    if (open) {
-      handleOpen();
-    }
-  }, [open]);
-
   const handleOpen = async () => {
-    setLoading(true);
-    const response = await fetch(
-      `http://localhost:3000/api/items?id=${list.id}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    const responseData = await response.json();
-    setData(responseData.data);
-    setLoading(false);
+    if (list._count.items !== 0 && list.status === Role.COMPLETED) {
+      setOpen(!open);
+    }
   };
+
   return (
     <div
-      className={`${styles.col12} ${style.ShoppingListCollapse}`}
-      onClick={() => setOpen(!open)}
+      className={`${styles.col12} ${styles.action_box} ${
+        open && styles.action_box_open
+      }`}
+      onClick={handleOpen}
     >
-      <span className={`${styles.row} ${style.banner}`}>
-        <p className={styles.pXLarge}>{list.name}</p>
-        <p className={styles.pXLarge}>
-          {loading && !data ? (
-            <Loading />
-          ) : list.status === Role.ACTIVE ? (
-            <Link
-              style={{ marginLeft: "auto" }}
-              href={`shoppingList/${list.id}`}
-            >
-              {">"}
-            </Link>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <p className={`${styles.pXLarge} ${styles.mrAuto}`}>{list.name}</p>
+        <StatusUpdate id={Number(list.id)} status={list.status} />
+        <p className={`${styles.pXLarge} ${styles.ml2}`}>
+          {list.status === Role.ACTIVE ? (
+            <Link href={`shoppingList/${list.id}`}>EDIT</Link>
           ) : (
-            "VIEW"
+            <span className={`${styles.cInfo}`}>VIEW</span>
           )}
         </p>
-      </span>
-      <div style={{ display: open ? "block" : "none" }}>
-        {data && (
-          <div className={`${styles.row}`}>
-            {data.map((item: any, i: number) => (
-              <div className={`${styles.col12}`} key={i}>
-                <p>{item.name}</p>
-              </div>
-            ))}
-          </div>
-        )}
       </div>
+      <ShoppingListCollapsePart open={open} list={list} />
     </div>
   );
 };
 
 export default ShoppingListCollapse;
+
+const ShoppingListCollapsePart = ({
+  open,
+  list,
+}: {
+  open: boolean;
+  list: List;
+}) => {
+  const [data, setData] = useState<List[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const response = await fetch(
+        `http://localhost:3000/api/items?id=${list.id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const responseData = await response.json();
+      setData(responseData.data);
+      setLoading(false);
+    };
+
+    if (open) {
+      fetchData();
+    }
+  }, [open, list.id]);
+
+  return (
+    <div style={{ display: open ? "block" : "none" }}>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className={styles.row}>
+          {data?.map((item: List, i: number) => (
+            <div className={`${styles.col12} ${styles.px3}`} key={i}>
+              <p>{item.name}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
