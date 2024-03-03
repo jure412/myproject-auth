550f4f1ba8851f0bbbe6194f1536b91152ab8bca
jure412 <jur3curk@gmail.com>
2024-03-03 10:40:41 +0100

    prisma models update

---
+++ prisma/migrations/20240303093527_init/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `id_token` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `refresh_token` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `session_state` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `VerificationToken` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Account" DROP COLUMN "id_token",
+DROP COLUMN "refresh_token",
+DROP COLUMN "session_state";
+
+-- DropTable
+DROP TABLE "Session";
+
+-- DropTable
+DROP TABLE "VerificationToken";
