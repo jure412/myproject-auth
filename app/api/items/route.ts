058a38c2019c5bbfab75664bbe6faa62f319d1c2
jure412 <jur3curk@gmail.com>
2024-03-03 10:20:43 +0100

    items | shopping list apis update

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -1,23 +1,61 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/app/prisma";
+import { NotificationType } from "@/enums";
+import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
-const prisma = new PrismaClient();
+import { z } from "zod";
 
-export async function GET(request: NextRequest) {
+const exportShoppingListValidation = z.object({
+  id: z.string().min(1, "Shopping list doesn't exists."),
+  token: z.string().min(20, "Unauthorize user."),
+});
+
+export async function GET(request: NextRequest, response: NextResponse) {
   const id = request.nextUrl.searchParams.get("id");
-  if (id) {
-    const data = await prisma.item.findMany({
-      where: {
-        shoppingListId: Number(id),
-      },
-      orderBy: { id: "desc" },
+  const type = request.nextUrl.searchParams.get("type");
+  const token = await getToken({ req: request, raw: true });
+
+  const validationRes = await exportShoppingListValidation.safeParseAsync({
+    id,
+    token,
+  });
+
+  if (!validationRes.success) {
+    const validationErrors = validationRes.error.errors.map(
+      ({ message }: { message: string }) => ({
+        message,
+        type: NotificationType.DANGER,
+      })
+    );
+    return NextResponse.json({
+      error: validationErrors,
+      success: false,
     });
-    if (data) {
-      return NextResponse.json({
-        data: data,
-        success: true,
-      });
-    }
   }
+
+  const data = await prisma.item.findMany({
+    where: {
+      shoppingListId: Number(id),
+    },
+    orderBy: { id: "desc" },
+  });
+  if (data) {
+    if (type === "json") {
+      const jsonString = JSON.stringify(data, null, 2);
+      const response = new NextResponse(jsonString);
+      response.headers.set(
+        "Content-disposition",
+        "attachment; filename=data.json"
+      );
+      response.headers.set("Authorization", `Bearer ${token}`);
+
+      return response;
+    }
+    return NextResponse.json({
+      data: data,
+      success: true,
+    });
+  }
+
   return NextResponse.json({
     error: ["Error while querying data"],
     success: false,
--- app/api/shoppingList/[id]/route.ts
+++ app/api/shoppingList/[id]/route.ts
@@ -1,34 +1,150 @@
 import { prisma } from "@/app/prisma";
-import { NextResponse } from "next/server";
+import { NotificationType } from "@/enums";
+import { NextApiRequest } from "next";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
 interface GetProps {
   params: {
     id: number;
   };
 }
+const ShoppingListValidate = z.object({
+  id: z.string().min(1, "Item doesn't exists"),
+});
+export async function GET(
+  request: NextApiRequest,
+  { params: { id } }: GetProps
+) {
+  const validationRes = await ShoppingListValidate.safeParseAsync({
+    id,
+  });
 
-export async function GET(request: Request, { params: { id } }: GetProps) {
-  if (id) {
-    const data = await prisma.shoppingList.findFirst({
-      where: {
-        id: Number(id),
+  if (!validationRes.success) {
+    const validationErrors = validationRes.error.errors.map(
+      ({ message }: { message: string }) => ({
+        message,
+        type: NotificationType.DANGER,
+      })
+    );
+    return { error: validationErrors, success: false };
+  }
+
+  const data = await prisma.shoppingList.findFirst({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      items: {
+        orderBy: { id: "desc" },
       },
-      include: {
-        items: {
-          orderBy: { id: "desc" },
-        },
-      },
+    },
+  });
+  if (data) {
+    return NextResponse.json({
+      data: data,
+      success: true,
     });
-    if (data) {
-      return NextResponse.json({
-        data: data,
-        success: true,
-      });
-    }
   }
 
   return NextResponse.json({
     error: ["Error while querying data"],
     success: true,
+  });
+}
+
+function compareArrays(arr1: {}[], arr2: {}[], propertyName: string) {
+  const matchingValues: string[] = [];
+
+  // Create a Set of values from arr2 for quicker lookups
+  const set = new Set(arr2.map((obj: any) => obj[propertyName]));
+
+  // Iterate over arr1 and check if each element's propertyName exists in arr2
+  arr1.forEach((obj: any) => {
+    if (set.has(obj[propertyName])) {
+      matchingValues.push(obj[propertyName]);
+    }
+  });
+
+  return matchingValues;
+}
+const importShoppingListValidation = z.object({
+  id: z.string().min(1, "Shoipping list doesn't exists"),
+  file: z.string().min(20, "file not provided"),
+});
+export async function POST(request: NextRequest, { params: { id } }: GetProps) {
+  const token = await getToken({ req: request });
+  const { file } = await request.json();
+
+  const validationRes = await importShoppingListValidation.safeParseAsync({
+    id,
+    file,
+  });
+
+  if (!validationRes.success) {
+    const validationErrors = validationRes.error.errors.map(
+      ({ message }: { message: string }) => ({
+        message,
+        type: NotificationType.DANGER,
+      })
+    );
+    return NextResponse.json({
+      error: validationErrors,
+      success: false,
+    });
+  }
+
+  let base64String = "";
+  if (file.includes(",")) {
+    base64String = file.split(",")[1];
+  } else {
+    base64String = file;
+  }
+
+  const decodedData = atob(base64String);
+  const jsonData = JSON.parse(decodedData);
+  const data = await prisma.shoppingList.findFirst({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      items: {
+        orderBy: { id: "desc" },
+      },
+    },
+  });
+  if (data) {
+    let matchingNames = compareArrays(data?.items!, jsonData, "name");
+    if (matchingNames.length === 0) {
+      const d = jsonData.map((item: any) => ({
+        ...item,
+        shoppingListId: Number(id),
+        userId: token?.id,
+      }));
+
+      const createManyItems = await prisma.item.createMany({ data: d });
+      return NextResponse.json({
+        data: createManyItems,
+        success: true,
+      });
+    }
+    const error = matchingNames.map((matchingName: any) => ({
+      type: NotificationType.DANGER,
+      message: `Json file includes marching element ${matchingName}`,
+    }));
+    return NextResponse.json({
+      error: error,
+      success: false,
+    });
+  }
+  return NextResponse.json({
+    error: [
+      {
+        type: NotificationType.DANGER,
+        message: "No shopping list found with this id",
+      },
+    ],
+    success: false,
   });
 }
