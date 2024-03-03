4e16ce87abddb2f5c4f1eb7ff7f69bf0a1eba1cf
jure412 <jur3curk@gmail.com>
2024-03-03 10:28:55 +0100

    shopping list pages upadate

--- app/shoppingList/[id]/page.tsx
+++ app/shoppingList/[id]/page.tsx
@@ -1,36 +1,47 @@
+import ExportJsonButton from "@/app/components/Button/Variants/ExportJsonButton";
 import ItemCheck from "@/app/components/ItemCheck/ItemCheck";
+import JsonFileDataImport from "@/app/components/JsonFileDataImport/JsonFileDataImport";
 import ModalButton from "@/app/components/Modal/ModalComponents/ModalButton";
 import StatusUpdate from "@/app/components/StatusUpdate/StatusUpdate";
 import { ModalType } from "@/enums";
+import { headers } from "next/headers";
+import styles from "../../global.module.scss";
 
 const ShoppingItem = async ({ params }: any) => {
-  // console.log(params.id);
   const res = await fetch(
     "http://localhost:3000/api/shoppingList/" + params.id,
     {
+      cache: "no-cache",
       method: "GET",
-      headers: {
-        "content-type": "application/json",
-      },
+      headers: headers(),
     }
   );
   const data = await res.json();
-
   return (
-    <>
-      <ModalButton modalType={ModalType.CREATE_NEW_ITEM}>
-        Add new Item
-      </ModalButton>
-      <h1>{data?.name}</h1>
-      <StatusUpdate id={params.id} status={data.data.status} />
-      <h2>List</h2>
-      <div>
-        {data &&
-          data?.data.items?.map((item: any, i: number) => (
-            <ItemCheck item={item} key={item.id} />
-          ))}
+    <div className={styles.row}>
+      <div className={styles.col12}>
+        <h1 className={`${styles.mt3} ${styles.mb4}`}>
+          Shopping List - {data.data.name}
+        </h1>
+
+        <ModalButton modalType={ModalType.CREATE_NEW_ITEM}>
+          Add new Item
+        </ModalButton>
+        <div className={`${styles.my3}`}>
+          <JsonFileDataImport id={params.id} />
+        </div>
+        <ExportJsonButton id={params.id} />
+        <h1>{data?.name}</h1>
+        <StatusUpdate id={params.id} status={data.data.status} />
+        <h2>List</h2>
+        <div>
+          {data &&
+            data?.data.items?.map((item: any, i: number) => (
+              <ItemCheck item={item} key={item.id} />
+            ))}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 export default ShoppingItem;
--- app/shoppingList/page.tsx
+++ app/shoppingList/page.tsx
@@ -1,30 +1,46 @@
 import { ModalType } from "@/enums";
+import { headers } from "next/headers";
 import ModalButton from "../components/Modal/ModalComponents/ModalButton";
-import ShoppingListCollapse from "../components/ShoppingListCollapse/ShoppingListCollapse";
+import ShoppingListCollapse, {
+  List,
+} from "../components/ShoppingListCollapse/ShoppingListCollapse";
+import styles from "../global.module.scss";
 
 const ShoppingList = async () => {
   const res = await fetch("http://localhost:3000/api/shoppingList", {
+    cache: "no-cache",
     method: "GET",
-    headers: {
-      "content-type": "application/json",
-    },
+    // I'm not sure way of passing the header would work in production.
+    headers: headers(),
   });
   const data = await res.json();
+
   return (
-    <>
-      <ModalButton
-        isDisabled={!data.data.isActiveOpen}
-        modalType={ModalType.CREATE_NEW_SHOPPING_LIST}
-      >
-        Create New Shopping List
-      </ModalButton>
-      <div>
-        {data &&
-          data?.data.shoppingLists?.map((list: any, i: number) => (
-            <ShoppingListCollapse key={i} list={list} />
-          ))}
+    <div className={styles.row}>
+      <div className={styles.col}>
+        <h1 className={styles.mt3}>Shopping Lists</h1>
+        <div className={styles.my2}>
+          {!data?.data?.isActiveOpen && (
+            <p>
+              Before Creating new Shopping list, all Shopping lists need to be
+              marked as COMPLETED
+            </p>
+          )}
+          <ModalButton
+            isDisabled={!data?.data?.isActiveOpen}
+            modalType={ModalType.CREATE_NEW_SHOPPING_LIST}
+          >
+            Create New Shopping List
+          </ModalButton>
+        </div>
+        <div>
+          {data &&
+            data?.data.shoppingLists?.map((list: List, i: number) => (
+              <ShoppingListCollapse key={i} list={list} />
+            ))}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 export default ShoppingList;
